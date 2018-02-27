ab16bf1b3ff44c6d5cadaa84621840edc764f537
KaichingChang <kaichingc@gmail.com>
2018-02-27 16:16:22 +0800

    Initial Commit

---
+++ SimpleGame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleGame
+//
+//  Created by 張凱慶 on 2018/2/27.
+//  Copyright © 2018年 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimpleGame/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleGame
+//
+//  Created by 張凱慶 on 2018/2/27.
+//  Copyright © 2018年 kaiching. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
